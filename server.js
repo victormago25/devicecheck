e30ef38a940135f418561b564f369eabc69dcf7b
Alonso Salas <a.salmo@gmail.com>
2013-07-28 22:01:54 -0600

    adding some logic

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname));
+app.use(app.router);
+//app.get('/:file', function(req, res){
+//    var file = req.params.file;
+//    res.sendfile(file);
+//});
+app.listen(80);
