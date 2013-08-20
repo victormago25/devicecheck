7b4314a2eda2c02a129f95b32ab7ab156f9e4eed
Alonso Salas <a.salmo@gmail.com>
2013-08-20 17:14:09 -0600

    Adding save logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,42 +2,43 @@
 /*global angular, window, console */
 var deviceBasePath = 'stock/';
 
-function ListCtrl($scope, time, angularFire, fbURL) {
-    angularFire(fbURL + deviceBasePath, $scope, 'stocks', {}).
-        then(function () {
-            angular.forEach($scope.stocks, function (element, id) {
+function ListCtrl($scope, time, angularFire, angularFireCollection, fbURL) {
+//    angularFire(fbURL + deviceBasePath, $scope, 'stocks', {}).
+//        then(function () {
+//            angular.forEach($scope.stocks, function (element, id) {
+//                element.password = '';
+//                element.$id = id;
+//            }, $scope.stocks);
+//        });
+    $scope.stocks = angularFireCollection(fbURL + deviceBasePath, function(dataSnapshot) {
+        angular.forEach(dataSnapshot.val(), function (element, id) {
                 element.password = '';
                 element.$id = id;
             }, $scope.stocks);
-        });
+    });
     $scope.time = time;
     $scope.angularFire = angularFire;
     $scope.fbURL = fbURL;
-    $scope.send = function (id) {
-        var deviceRef = new Firebase($scope.fbURL + deviceBasePath + id);
+    $scope.send = function (index) {
+        var deviceRef = new Firebase($scope.fbURL + deviceBasePath + $scope.stocks[index].$id);
 //        var current = _.filter($scope.stocks, function (device) {
 //            return device.$id === id;
 //        });
         deviceRef.once('value', function(dataSnapshot) {
-            if (dataSnapshot.child('inUse').val() && (dataSnapshot.child('lockPhrase').val() === $scope.stocks[id].password) && (dataSnapshot.child('user').val() === $scope.stocks[id].user)) {
-                $scope.stocks[id].password = '';
-                deviceRef.update({inUse: false, lockPhrase: '', user: ''});
+            if (dataSnapshot.child('inUse').val() && (dataSnapshot.child('lockPhrase').val() === $scope.stocks[index].password) && (dataSnapshot.child('user').val() === $scope.stocks[index].user)) {
+                $scope.stocks[index].password = '';
+                $scope.stocks[index].user = '';
+                $scope.stocks[index].lockPhrase = '';
+                $scope.stocks[index].inUse = false;
+                $scope.stocks.update($scope.stocks[index].$id);
             } else if (!dataSnapshot.child('inUse').val() && (dataSnapshot.child('lockPhrase').val() === '') && (dataSnapshot.child('user').val() === '')) {
-                $scope.stocks[id].password = '';
-                var hash = CryptoJS.MD5($scope.stocks[id].lockPhrase);
-                deviceRef.update({inUse: true, lockPhrase: $scope.stocks[id].lockPhrase, user: $scope.stocks[id].user});
+                $scope.stocks[index].inUse = true;
+                $scope.stocks[index].lockPhrase = $scope.stocks[index].password;
+//                $scope.stocks[index].lockPhrase = CryptoJS.MD5($scope.stocks[index].password);
+                $scope.stocks[index].password = '';
+                $scope.stocks.update($scope.stocks[index].$id);
             }
         });
-//        $scope.angularFire($scope.fbURL + deviceBasePath + id, $scope, 'remote', {}).
-//            then(function () {
-//                var current = _.filter($scope.stocks, function (device) {
-//                    return device.$id === id;
-//                });
-//                if ($scope.remote.lockPhrase === current[0].lockPhrase) {
-//                    $scope.remote.inUse = false;
-//                    console.log('saved');
-//                }
-//            });
     };
 }
 
