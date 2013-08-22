815c795de967e13db8caeca4adfdc9ff734a357d
Alonso Salas <a.salmo@gmail.com>
2013-08-22 17:54:08 -0600

    Adding active tab and dynamic info

--- mau/app.js
+++ mau/app.js
@@ -1,8 +1,20 @@
 /*jslint sloppy: true */
 /*global angular, window, console */
 
-function TodoCtrl($scope) {
-    $scope.todos = [
-        {text: 'learn angular', done: true},
-        {text: 'build an angular app', done: false}];
+angular.module('workshop01', []);
+
+function returnTopics() {
+    var topics = [
+        {text: 'HTML', class: 'active'},
+        {text: 'CSS'},
+        {text: 'Javascript'},
+        {text: 'JQuery'},
+        {text: 'Angular'}],
+        maxTopics = _.random(1, 5);
+    console.log(maxTopics);
+    return topics.slice(0, maxTopics);
+}
+
+function TopicsCtrl($scope) {
+    $scope.topics = returnTopics();
 }
