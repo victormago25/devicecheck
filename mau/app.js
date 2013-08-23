bafd00dc724627a7bf278091ee986c81abd0f0f5
Alonso Salas <a.salmo@gmail.com>
2013-08-22 21:58:55 -0600

    adding list logic

--- mau/app.js
+++ mau/app.js
@@ -1,20 +1,71 @@
 /*jslint sloppy: true */
-/*global angular, window, console */
+/*global angular, window, console, _ */
 
-angular.module('workshop01', []);
+angular.module('workshop01', []).
+    directive('workshop01.directives.tab', function () {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {},
+            controller: function ($scope, $element) {
+                var panes = $scope.panes = [];
 
-function returnTopics() {
-    var topics = [
-        {text: 'HTML', class: 'active'},
+                $scope.select = function (pane) {
+                    angular.forEach(panes, function (pane) {
+                        pane.selected = false;
+                    });
+                    pane.selected = true;
+                };
+                this.addPane = function (pane) {
+                    if (panes.length === 0) {
+                        $scope.select(pane);
+                    }
+                    panes.push(pane);
+                };
+            },
+            template:
+                    '<div class="tabbable">' +
+                    '<ul class="nav nav-tabs">' +
+                    '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">' +
+                    '<a hrefs="" ng-click="select(pane)">{{pane.title}}</a>' +
+                    '</li>' +
+                    '</ul>' +
+                    '<div class="tab-content" ng-transclude></div>' +
+                    '</div>',
+            replace: true
+        };
+    }).
+    directive('pane', function () {
+        return {
+            require: '^tabs',
+            restrict: 'E',
+            transclude: true,
+            scope: {title: '@'},
+            link: function (scope, element, attrs, tabsCtrl) {
+                tabsCtrl.addPane(scope);
+            },
+            template:
+                    '<div class="tab-pane" ng-class="{active: selected}" ng-transclude>' +
+                    '</div>',
+            replace: true
+        };
+    });
+
+function returnGroups() {
+    var groups = [
+        {text: 'HTML', classes: 'active'},
         {text: 'CSS'},
         {text: 'Javascript'},
         {text: 'JQuery'},
         {text: 'Angular'}],
-        maxTopics = _.random(1, 5);
-    console.log(maxTopics);
-    return topics.slice(0, maxTopics);
+        maxGroups = _.random(1, 5);
+    console.log(maxGroups);
+    return groups.slice(0, maxGroups);
 }
 
 function TopicsCtrl($scope) {
-    $scope.topics = returnTopics();
+    $scope.refresh = function () {
+        $scope.groups = returnGroups();
+    };
+    $scope.topics = _.range(10);
 }
