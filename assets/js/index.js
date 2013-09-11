427b572a7e85592a7cd8e584d95965fb4bf175dc
Alonso Salas <a.salmo@gmail.com>
2013-09-11 16:42:12 -0600

    Adding latest fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,10 @@ angular.module('devicechecker.directives', [])
                         { "mData": "user" },
                         { "mData": "status" },
                         { "mData": "date" }],
-                        "aaSorting": [[ 2, "desc" ]]}).fnAddData(scope.stocks[attrs.activeTable].history);
+                        "aaSorting": [[ 2, "desc" ]]});
+                    if(scope.stocks[attrs.activeTable].history) {
+                        element.dataTable().fnAddData(scope.stocks[attrs.activeTable].history);
+                    }
                 }
             }
         };
@@ -34,18 +37,29 @@ angular.module('device', ['ui.bootstrap', 'firebase', 'devicechecker.directives'
         $scope.send = function (index) {
             var deviceRef = new Firebase($scope.fbURL + deviceBasePath + $scope.stocks[index].$id);
             deviceRef.once('value', function (dataSnapshot) {
-                var currentEncryptPass = CryptoJS.MD5($scope.stocks[index].password).toString();
+                var currentEncryptPass = CryptoJS.MD5($scope.stocks[index].password).toString(),
+                    newRecord = {
+                        user: $scope.stocks[index].user,
+                        status: 'Checked-in',
+                        date: new Date().toString()
+                    };
+                if (!$scope.stocks[index].history) {
+                    $scope.stocks[index].history = [];
+                } else {
+                    $scope.stocks[index].history = dataSnapshot.child('history').val();
+                }
                 if (dataSnapshot.child('inUse').val() && (dataSnapshot.child('lockPhrase').val() === currentEncryptPass) && (dataSnapshot.child('user').val() === $scope.stocks[index].user)) {
                     $scope.stocks[index].password = '';
                     $scope.stocks[index].user = '';
                     $scope.stocks[index].lockPhrase = '';
-                    $scope.stocks[index].history = dataSnapshot.child('history').val();
+                    $scope.stocks[index].history.push(newRecord);
                     $scope.stocks[index].inUse = false;
                     $scope.stocks.update($scope.stocks[index].$id);
                 } else if (!dataSnapshot.child('inUse').val() && (dataSnapshot.child('lockPhrase').val() === '') && (dataSnapshot.child('user').val() === '')) {
+                    newRecord.status = 'Checked-out';
                     $scope.stocks[index].inUse = true;
                     $scope.stocks[index].lockPhrase = currentEncryptPass;
-                    $scope.stocks[index].history = dataSnapshot.child('history').val();
+                    $scope.stocks[index].history.push(newRecord);
                     $scope.stocks[index].password = '';
                     $scope.stocks.update($scope.stocks[index].$id);
                 }
